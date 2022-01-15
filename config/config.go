a52b2360c07a5bed74421f83b4dcc9460b3f6744
ayo-ajayi <ajayiayomide247@gmail.com>
2022-01-15 20:02:37 +0100

    comments added

--- config/config.go
+++ config/config.go
@@ -6,17 +6,20 @@ import (
 	"os"
 )
 
-func Config() (string, string, string) {
+//This is a wrapper that takes the port, mongo_url and auth secret to return them for use on a global scale.
+//Set first argument to empty string "" to use default port 8080.
+func Config(port string, mongo_url string, secret string) (string, string, string) {
+	
 	err := godotenv.Load(".env")
 
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
+	port_ := os.Getenv(port)
+	if port_ == "" {
+		port_ = "8080"
 	}
-	mongo_url := os.Getenv("MONGODB_URL")
-	secret := os.Getenv("ACCESS_SECRET")
-	return port, mongo_url, secret
+	mongo_url_ := os.Getenv(mongo_url)
+	secret_ := os.Getenv(secret)
+	return port_, mongo_url_, secret_
 }
